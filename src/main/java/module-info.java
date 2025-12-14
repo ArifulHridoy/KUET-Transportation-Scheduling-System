de87034844e6b7c5ccccdc900ac10ddd05878785
Ariful Islam Sheikh <sheikh2207051@stud.kuet.ac.bd>
2025-12-14 19:38:23 +0600

    Final project structure created, deleted previous stucture.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
-module com.example.trscsy {
+module com.example.kuet_transportation_and_schedueling_system {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires javafx.base;
+    requires java.sql;
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
@@ -11,9 +13,10 @@ module com.example.trscsy {
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
-    exports com.example.trscsy;
-    exports com.example.trscsy.model;
-
-    opens com.example.trscsy to javafx.fxml;
-    opens com.example.trscsy.controller to javafx.fxml;
+    opens com.example.kuet_transportation_and_schedueling_system to javafx.fxml;
+    opens com.example.kuet_transportation_and_schedueling_system.controller to javafx.fxml;
+    opens com.example.kuet_transportation_and_schedueling_system.model to javafx.base;
+    exports com.example.kuet_transportation_and_schedueling_system;
+    exports com.example.kuet_transportation_and_schedueling_system.controller;
+    exports com.example.kuet_transportation_and_schedueling_system.model;
 }
